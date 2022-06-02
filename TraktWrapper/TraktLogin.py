5408eccda4c1a1b0eeb7fac21e59d85bdbc32e77
Aymen Djellal <aymendjellal@gmail.com>
2022-06-02 03:25:02 -0700

    Adding auto token refresh save

--- TraktWrapper/TraktLogin.py
+++ TraktWrapper/TraktLogin.py
@@ -6,8 +6,13 @@ def Login(traktUser : TraktUser):
     traktUser.TraktClientWrapper = TraktClient()
     traktUser.TraktClientWrapper.configuration.defaults.client(
         traktUser.ClientId, traktUser.ClientSecret)
-    traktUser.TraktClientWrapper.configuration.defaults.oauth.from_response(
+    oath = traktUser.TraktClientWrapper.configuration.defaults.oauth.from_response(
         authenticate(traktUser.Authorisation), refresh=True)
+    if(oath != None and oath.data["oauth.token"] != None):
+        traktUser.Authorisation["access_token"] = oath.data["oauth.token"]
+        traktUser.Authorisation["refresh_token"] = oath.data["oauth.refresh_token"]
+        traktUser.Authorisation["created_at"] = oath.data["oauth.created_at"]
+        traktUser.Authorisation["expires_in"] = oath.data["oauth.expires_in"]
 
 def authenticate(authorization):
     if authorization:
